ed6cd62be970da9c5fc72744b27a4d46af11e895
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2020-10-16 13:25:30 +1100

    layout signup component, import boostrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import './App.css';
+import SignUp from './SignUp';
 
 function App() {
   return (
-    "hello world"
-  );
+    <Container 
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: '100vh'}}
+    >
+      <div className="w-100" style={{ maxWidth: "400px"}}>
+        <SignUp/>
+      </div>
+    </Container>
+  )
+  
 }
 
 export default App;
