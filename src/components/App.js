f61274ee1cf726b6a001dffd911279904af50796
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2020-10-20 19:17:59 +1100

    create PrivateRoute, use PrivateRoute for dashboard

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import Signup from './Signup';
 import Login from './Login';
 import Dashboard from './Dashboard';
+import PrivateRoute from './PrivateRoute';
 
 function App() {
   return (
@@ -17,7 +18,7 @@ function App() {
         <div className="w-100" style={{ maxWidth: "400px"}}>
           <Router>
             <Switch>
-              <Route exact path="/" component={Dashboard}/>
+              <PrivateRoute exact path="/" component={Dashboard}/>
               <Route path="/login" component={Login}/>
               <Route path="/signup" component={Signup}/>
             </Switch>
